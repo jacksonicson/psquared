de3bd5502815a5f6e3cf0b8de9d080df51772394
Andreas <jacksonicson@gmail.com>
2013-05-01 22:35:26 +0200

    Tried different variations in the test program. Seems to work pretty stable. MSE calculations for verification for many runs is still missing.

--- test/de/tum/in/dss/psquare/TestPercentile.java
+++ test/de/tum/in/dss/psquare/TestPercentile.java
@@ -1,6 +1,5 @@
 package de.tum.in.dss.psquare;
 
-import java.util.Arrays;
 import java.util.Random;
 
 import org.junit.Test;
@@ -17,10 +16,11 @@ public class TestPercentile {
 	@Test
 	public void testAccept() {
 		float[] observations = { 3, 4, 5, 6, 2 };
-		Percentile p = new Percentile(0.99f, observations);
+		float percentile= 0.75f; 
+		Percentile p = new Percentile(percentile, observations);
 
 		// Accept all test values
-		float[] test = new float[50000];
+		float[] test = new float[10000];
 		Random rand = new Random();
 		for (int i = 0; i < test.length; i++) {
 			test[i] = Math.abs(rand.nextFloat() * 1000);
@@ -28,21 +28,18 @@ public class TestPercentile {
 		double r = 0;
 		for (float t : test) {
 			r = p.accept(t);
-			// System.out.println("p=" + r);
-			// p.dump();
 		}
 
 		System.out.println("r: " + r);
 
 		org.apache.commons.math3.stat.descriptive.rank.Percentile p2 = new org.apache.commons.math3.stat.descriptive.rank.Percentile(
-				99);
+				percentile * 100);
 		double[] dall = new double[observations.length + test.length];
 		for (int i = 0; i < observations.length; i++)
 			dall[i] = observations[i];
 		for (int i = 0; i < test.length; i++)
 			dall[observations.length + i] = test[i];
 
-		
 		System.out.println("Compare: " + p2.evaluate(dall) + " vs " + r);
 	}
 }
