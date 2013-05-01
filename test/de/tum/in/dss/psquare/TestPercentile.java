011416dc47bd1e277019fed1518a91e3c00b0536
Andreas <jacksonicson@gmail.com>
2013-05-01 20:55:20 +0200

    Added a test program that compares the estimated percentiel with the one calculated by Apache math libs. Seems to work for larger inputs but not for very small inputs [0,1]. Float precision might be a cause.

---
+++ test/de/tum/in/dss/psquare/TestPercentile.java
@@ -0,0 +1,48 @@
+package de.tum.in.dss.psquare;
+
+import java.util.Arrays;
+import java.util.Random;
+
+import org.junit.Test;
+
+public class TestPercentile {
+
+	@Test
+	public void testCreation() {
+		float[] observations = { 3, 4, 5, 6, 2 };
+		Percentile p = new Percentile(0.99f, observations);
+		p.dump();
+	}
+
+	@Test
+	public void testAccept() {
+		float[] observations = { 3, 4, 5, 6, 2 };
+		Percentile p = new Percentile(0.99f, observations);
+
+		// Accept all test values
+		float[] test = new float[50000];
+		Random rand = new Random();
+		for (int i = 0; i < test.length; i++) {
+			test[i] = Math.abs(rand.nextFloat() * 1000);
+		}
+		double r = 0;
+		for (float t : test) {
+			r = p.accept(t);
+			// System.out.println("p=" + r);
+			// p.dump();
+		}
+
+		System.out.println("r: " + r);
+
+		org.apache.commons.math3.stat.descriptive.rank.Percentile p2 = new org.apache.commons.math3.stat.descriptive.rank.Percentile(
+				99);
+		double[] dall = new double[observations.length + test.length];
+		for (int i = 0; i < observations.length; i++)
+			dall[i] = observations[i];
+		for (int i = 0; i < test.length; i++)
+			dall[observations.length + i] = test[i];
+
+		
+		System.out.println("Compare: " + p2.evaluate(dall) + " vs " + r);
+	}
+}
