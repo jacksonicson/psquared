3dc2b4f4be4526023227057d96246f8d3d2d8635
Andreas Wolke <jacksonicson@gmail.com>
2013-05-06 13:02:02 +0200

    Added corner caess test to suite

--- test/de/tum/in/dss/psquare/PSquaredSuite.java
+++ test/de/tum/in/dss/psquare/PSquaredSuite.java
@@ -5,7 +5,7 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ TestRandom.class, TestPaper.class })
+@SuiteClasses({ TestRandom.class, TestPaper.class, TestCornerCases.class })
 public class PSquaredSuite {
 
 }
